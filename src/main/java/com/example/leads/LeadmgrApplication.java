7d90caed48040996f843c17dcce7cd62c83dd3e1
natsuki <n.kaido@ichirirop.com>
2025-06-29 12:15:01 +0900

    スケルトン

---
+++ src/main/java/com/example/leads/LeadmgrApplication.java
@@ -0,0 +1,13 @@
+package com.example.leads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeadmgrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeadmgrApplication.class, args);
+	}
+
+}
