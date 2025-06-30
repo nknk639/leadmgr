4877b55def05e996eb7ec13f26e6b8494823e140
natsuki <n.kaido@ichirirop.com>
2025-06-30 20:34:07 +0900

    lazyに変更

--- src/main/java/com/example/leads/repository/CallHistoryRepository.java
+++ src/main/java/com/example/leads/repository/CallHistoryRepository.java
@@ -2,11 +2,13 @@ package com.example.leads.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.leads.model.CallHistory;
 import com.example.leads.model.Lead;
 
 public interface CallHistoryRepository extends JpaRepository<CallHistory, Long> {
+	@EntityGraph(attributePaths = {"resultCode", "prevResultCode"})
     List<CallHistory> findByLeadOrderByCallDatetimeDesc(Lead lead);
 }
