e223bcf4c4247ea4f302e86bd2735287f373906c
natsuki <n.kaido@ichirirop.com>
2025-07-03 22:29:48 +0900

    セキュリティ更新

--- src/main/java/com/example/leads/config/SecurityConfig.java
+++ src/main/java/com/example/leads/config/SecurityConfig.java
@@ -9,6 +9,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
